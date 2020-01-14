124b08dd9b889315c9262c948f77ca7a64014d80
WangZT01 <18640096737@163.com>
2020-01-14 15:52:50 +0800

    添加项目文件。

---
+++ Models/Movie.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace RazorPagesMovie.Models
+{
+    public class RazorPagesMovieContext : DbContext
+    {
+        public RazorPagesMovieContext(DbContextOptions<RazorPagesMovieContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<RazorPagesMovie.Models.Movie> Movie { get; set; }
+    }
+    public class Movie
+    {
+        public int ID
+        {
+            get;
+            set;
+        }
+        public string Title
+        {
+            get;
+            set;
+        }
+
+        [DataType(DataType.Date)]
+        public DateTime ReleaseDate
+        {
+            get;
+            set;
+        }
+
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+    }
+}
